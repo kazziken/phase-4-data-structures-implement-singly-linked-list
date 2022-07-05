aa06dd74477d3a79448f1f17dd17eca0c09f0a04
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-07-05 15:42:04 -0400

    commit tree afa4a90620ab81a6dbae4420dfb398f70c26d503

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,38 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        node.next_node = head
+        self.head = node
+    end
+
+    def append(node)
+        # Add element to the beginning of the list if the list is empty
+        if head.nil?
+          self.head = node
+          return
+        end
+    
+        # Otherwise, traverse the list to find the last node
+        last_node = head
+        while last_node.next_node
+          last_node = last_node.next_node
+        end
+    
+        # and add the node to the end
+        last_node.next_node = node
+    end
+
+    def delete_head
+        head.pop
+    end
+
+
+
 end
